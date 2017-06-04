dbe2ba7427a840e96fb8c94b524dcb17e6965d38
suiyujin <suiyujin04@gmail.com>
2017-06-05 02:20:49 +0900

    add secrets.yml to git repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, ENV.fetch('DEPLOY_TO')
 set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", ".env"
+append :linked_files, "config/database.yml", ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
